3b8a9024e332654a5d3bf86ed227f483b6a13544
FauzanIbnuSarky <elfauzanclothes@gmail.com>
2017-12-04 17:14:33 +0700

    11

---
+++ Browser/Browser/MuslimViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MuslimViewController.swift
+//  Browser
+//
+//  Created by Master on 26/9/17.
+//  Copyright © 2017 Master. All rights reserved.
+//
+
+import UIKit
+
+class MuslimViewController: UIViewController {
+    @IBOutlet weak var webView: UIWebView!
+    let urlweb = "firanda.com"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let requestUrl = NSURL(string: urlweb)
+        
+        
+        let request = NSURLRequest(url: requestUrl! as URL)
+        
+        webView.loadRequest(request as URLRequest)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
